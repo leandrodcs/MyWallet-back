43af2c27969ae301ec6ca637b68c7e6b42bb3b5d
leandrodcs <eng.leandrodcs@gmail.com>
2021-10-22 15:11:09 -0300

    Sign-in tests implemented, only the one which includes a response has yet to be implemented

---
+++ tests/signin.test.js
@@ -0,0 +1,30 @@
+import supertest from "supertest";
+import connection from "../src/database/database.js";
+import { app } from "../src/app.js";
+
+afterAll(async () => {
+    connection.end();
+  });
+
+describe(`POST /sign-in`, () => {
+
+    it(`returns status 404`, async () => {
+        const body = {
+            email: "wrongEmail@email.com",
+            password: "permanentTester",
+        };
+        const result = await supertest(app).post(`/sign-in`).send(body);
+        const status = result.status;
+        expect(status).toEqual(404);
+    });
+
+    it(`returns status 401`, async () => {
+        const body = {
+            email: "permanentTest@email.com",
+            password: "wrongPassword",
+        };
+        const result = await supertest(app).post(`/sign-in`).send(body);
+        const status = result.status;
+        expect(status).toEqual(401);
+    });
+});
